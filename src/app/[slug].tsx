59bf1a6783d71d06c80d29dbbeb9686d8af4df5f
Vadim Savin <savinvadim1312@gmail.com>
2024-02-28 16:54:45 +0000

    Thumbnails

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -44,6 +44,7 @@ const PostDetailsPage = () => {
         <Image
           source={{ uri: `/thumbnails/${post.thumbnail}` }}
           style={{ width: '100%', aspectRatio: 16 / 9 }}
+          alt={post.title}
         />
 
         <Markdown>{post.content}</Markdown>
