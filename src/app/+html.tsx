f33416bd210f6cfa133c92fd367fd00168935336
Vadim Savin <savinvadim1312@gmail.com>
2024-02-28 16:44:18 +0000

    page meta details: title, description

---
+++ src/app/+html.tsx
@@ -0,0 +1,37 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import type { PropsWithChildren } from 'react';
+import Head from 'expo-router/head';
+
+// This file is web-only and used to configure the root HTML for every
+// web page during static rendering.
+// The contents of this function only run in Node.js environments and
+// do not have access to the DOM or browser APIs.
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        {/*
+          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
+          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
+        */}
+        <ScrollViewStyleReset />
+
+        {/* Add any additional <head> elements that you want globally available on web... */}
+      </head>
+
+      <Head>
+        <title>My Blog Website</title>
+        <meta name="description" content="This is my blog." />
+      </Head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import { getPost, getAllPosts } from '../repository/postRepository';
 import Markdown from 'react-native-markdown-display';
+import Head from 'expo-router/head';
 
 export async function generateStaticParams(): Promise<
   Record<string, string>[]
@@ -21,22 +22,28 @@ const PostDetailsPage = () => {
   }
 
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-      }}
-      contentContainerStyle={{
-        maxWidth: 960,
-        width: '100%',
-        marginHorizontal: 'auto',
-        padding: 20,
-      }}
-    >
-      <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.description} />
+      </Head>
+      <ScrollView
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+        }}
+        contentContainerStyle={{
+          maxWidth: 960,
+          width: '100%',
+          marginHorizontal: 'auto',
+          padding: 20,
+        }}
+      >
+        <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
 
-      <Markdown>{post.content}</Markdown>
-    </ScrollView>
+        <Markdown>{post.content}</Markdown>
+      </ScrollView>
+    </>
   );
 };
 
